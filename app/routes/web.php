bc35db7c93272befe11472eb463d61a01ea791a0
Vadym <vadosdv00000@gmail.com>
2018-12-23 00:08:25 +0200

    created dashboard page

--- app/routes/web.php
+++ app/routes/web.php
@@ -3,3 +3,7 @@
 Route::get('/', "HomeController@index")->name('home');
 Auth::routes();
 Route::get('verify/{token}', 'Auth\RegisterController@verify')->name('register.verify');
+
+Route::group(['prefix' => 'admin-panel', 'as' => 'admin.', 'namespace' => 'Admin' ], function () {
+    Route::get('/', 'DashboardController@index')->name('dashboard');
+});
